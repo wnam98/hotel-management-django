da84b53f8ca74a6c60cd9888bb952b88f2e08096
wnam98 <49884140+wnam98@users.noreply.github.com>
2020-02-14 12:54:55 -0500

    "saved changes to url.py"

--- hotelwebapp/hotelwebapp/urls.py
+++ hotelwebapp/hotelwebapp/urls.py
@@ -1,4 +1,4 @@
-"""hotelwebapp URL Configuration
+    """hotelwebapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
