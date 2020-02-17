cbe92c853f9beaefe6e6253219370d3ffdeb822f
wnam98 <49884140+wnam98@users.noreply.github.com>
2020-02-16 21:14:35 -0500

    added template reservation model

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# hotel-management-django
-Hotel Reservation Web Application using django framework MVC architecture and SQL database.
+# Hostel-Managment-System
--- hms/wsgi.py
+++ hms/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for hotelwebapp project.
+WSGI config for hms project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotelwebapp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hms.settings")
 
 application = get_wsgi_application()
